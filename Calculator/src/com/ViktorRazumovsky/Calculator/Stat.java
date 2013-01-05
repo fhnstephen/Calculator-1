5ce8c0eb8aca90d4b51309aa3834a72e927b4223
viktor <possible947@gmail.com>
2013-01-05 15:32:43 +0200

    add memo function. not tested.

---
+++ Calculator/src/com/ViktorRazumovsky/Calculator/Stat.java
@@ -0,0 +1,12 @@
+package com.ViktorRazumovsky.Calculator;
+
+public class Stat {
+	public String str;
+	public boolean mem;
+	public boolean memC;
+	Stat(String arg1,boolean arg2,boolean arg3) {
+		str=arg1;
+		mem=arg2;
+		memC=arg3;
+	}
+}
